33a7b5a9d95d031972a12b1f87c7fb50382956e6
dev-manish301 <rajzmanish94@gmail.com>
2025-07-18 01:25:23 +0530

    updating files

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import FuzzyText from "../components/FuzzyText";
 
 function NotFound() {
+  const hoverIntensity = 0.5;
+  const enableHover = true;
+
   return (
-    <div>NotFound</div>
-  )
+    <div className="h-screen scroll-mb-0 flex justify-center items-center">
+      <FuzzyText
+        baseIntensity={0.2}
+        hoverIntensity={hoverIntensity}
+        enableHover={enableHover}
+      >
+        404
+      </FuzzyText>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;
