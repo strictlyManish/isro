4ee13a383b0ccf8e41b480cdd73ba797bcfe5946
dev-manish301 <rajzmanish94@gmail.com>
2025-07-20 14:11:50 +0530

    lazy loading

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useState } from "react";
-import { Menu, X } from "lucide-react";
+import { Menu} from "lucide-react";
+
 function NavigationBar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -10,8 +11,8 @@ function NavigationBar() {
 
   const navLinkClass = ({ isActive }) =>
     isActive
-      ? "text-orange-400  border-orange-500 pb-1"
-      : "text-white border-transparent pb-1 hover:border-orange-400";
+      ? "text-orange-400 border-b-2 border-orange-500 pb-1"
+      : "text-white border-b-2 border-transparent pb-1 hover:border-orange-400";
 
   const navItems = [
     { name: "Home", to: "/" },
@@ -24,7 +25,7 @@ function NavigationBar() {
   ];
 
   return (
-    <header className="w-full bg-transparent text-white px-4 sm:px-8 py-4">
+    <header className="w-full sticky top-0 z-50 bg-transparent backdrop-blur px-4 sm:px-8 py-4 transition-all duration-300">
       <div className="flex justify-between items-center">
         <img
           src="src/assets/favicon.png.webp"
@@ -32,17 +33,16 @@ function NavigationBar() {
           width="50"
           className="object-contain"
         />
+        {/* Desktop Navigation */}
         <nav className="hidden md:flex gap-6 font-medium text-lg">
           {navItems.map((item, idx) => (
-            <NavLink
-              key={idx}
-              to={item.to}
-              className={navLinkClass}
-            >
+            <NavLink key={idx} to={item.to} className={navLinkClass}>
               {item.name}
             </NavLink>
           ))}
         </nav>
+
+        {/* Mobile Menu Button */}
         <button
           className="md:hidden text-white"
           onClick={toggleMenu}
@@ -51,6 +51,8 @@ function NavigationBar() {
           {menuOpen ? <X size={28} /> : <Menu size={28} />}
         </button>
       </div>
+
+      {/* Mobile Navigation */}
       {menuOpen && (
         <nav className="flex flex-col gap-4 mt-4 md:hidden font-medium text-lg">
           {navItems.map((item, idx) => (
--- src/routes/Mainroutes.jsx
+++ src/routes/Mainroutes.jsx
@@ -1,28 +1,33 @@
-import { Route, Routes } from "react-router"
-import Home from "../pages/Home"
-import Engangements from "../pages/Engangements"
-import Programmes from "../pages/Programmes"
-import Resources from "../pages/Resources"
-import Services from "../pages/Services"
-import Activities from "../pages/Activities"
-import About from "../pages/About"
-import NotFound from "../pages/NotFound"
+import { Route, Routes } from "react-router";
+import { Suspense, lazy } from "react";
+
+const Home = lazy(() => import("../pages/Home"));
+const About = lazy(() => import("../pages/About"));
+const Activities = lazy(() => import("../pages/Activities"));
+const Services = lazy(() => import("../pages/Services"));
+const Programmes = lazy(() => import("../pages/Programmes"));
+const Resources = lazy(() => import("../pages/Resources"));
+const Engangements = lazy(() => import("../pages/Engangements"));
+const NotFound = lazy(() => import("../pages/NotFound"));
+const Loader = lazy(()=>import("../components/Loader"))
 
 function Mainroutes() {
   return (
-    <div>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/activities" element={<Activities/>}/>
-          <Route path="/services" element={<Services/>}/>
-          <Route path="/programmes" element={<Programmes/>}/>
-          <Route path="/resources" element={<Resources/>}/>
-          <Route path="/engangements" element={<Engangements/>}/>
-          <Route path="*"  element={<NotFound/>}/>
-        </Routes>
-    </div>
-  )
+    <Suspense fallback={<div className="text-center p-10">
+      <Loader/>
+    </div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/programmes" element={<Programmes />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/engangements" element={<Engangements />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default Mainroutes
+export default Mainroutes;
